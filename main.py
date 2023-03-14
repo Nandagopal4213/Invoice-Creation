c72ac73ccbb0d04194b8c6f5734376da374ffb6f
Nandagopal4213 <menandu2324@gmail.com>
2023-03-14 12:04:07 +0530

    Create a minimal for each excel file

--- main.py
+++ main.py
@@ -6,11 +6,25 @@ from pathlib import Path
 filepaths = glob.glob("Invoices/*xlsx")
 
 for filepath in filepaths:
-    df= pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P",unit="mm",format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_no = filepath.split("-")[0]
+    invoice_no,date = filepath.split("-")
+
     pdf.set_font(family="Times",size=16,style="B")
-    pdf.cell(w=50, h=8,txt=f"Invoices_no.{invoice_no}")
+    pdf.cell(w=50, h=8,txt=f"{invoice_no}",ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date:{date}")
+
+    df= pd.read_excel(filepath, sheet_name="Sheet 1")
+    for index, row in df.iterrows():
+        pdf.set_font()
+        pdf.set_text_color()
+        pdf.cell()
+
     pdf.output(f"PDFs/{filename}.pdf")
+
+
